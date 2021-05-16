b06509a9d7de1b9011ae75d10e1bc1c95f50ece9
enestatli <enes@thekompanion.com>
2021-05-16 22:23:46 +0300

    New: Added Label and Button

--- LearningSwift/ViewController.swift
+++ LearningSwift/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+ 
+    @IBOutlet weak var centerLabel: UILabel!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        count = count + 1
+        centerLabel.text = String(count)
+        
+        if(count >= 10){
+            view.backgroundColor = .purple
+        }
+//        view.backgroundColor = .green
+//        centerLabel.text = "I love you"
+//        centerLabel.textColor = .white
+          
+    }
+    
 }
 
